332ff5db92b6d4f3eb0289344f1d92f066114fa0
Abdullah Shishir <abdullahshishir786@gmail.com>
2026-05-18 03:34:10 +0600

    chore : db setup

--- src/main/java/com/store/creatorstore/CreatorStoreApplication.java
+++ src/main/java/com/store/creatorstore/CreatorStoreApplication.java
@@ -1,12 +1,16 @@
 package com.store.creatorstore;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CreatorStoreApplication {
-
     public static void main(String[] args) {
+
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach((entry)-> System.setProperty(entry.getKey(),entry.getValue()));
+
         SpringApplication.run(CreatorStoreApplication.class, args);
     }
 
